58a10d80b6e477e3c59547417abffffe6f69d0f5
Anurag Sharma <anuragsaitiya45@gmail.com>
2024-08-09 19:06:22 +0530

    update
    new program

--- src/Left_triangle.java
+++ src/Left_triangle.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class Left_triangle {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Give the number");
+        int num = sc.nextInt();
+
+      for(int i=1 ; i<= num; i++){
+
+          for(int z=1 ; z <= (num-i); z++){
+              System.out.print(" ");
+          }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+          }
+            System.out.println();
+      }
 
     }
 }
