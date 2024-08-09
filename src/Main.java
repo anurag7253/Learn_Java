27c6620fd62f2b30b5fc41b43cc6f146a1957676
Anurag Sharma <anuragsaitiya45@gmail.com>
2024-08-09 15:18:26 +0530

    new

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+//        String name = s.nextLine();
+//        int num = s.nextInt();
+//        System.out.println(num);
+//        System.out.println(name);
+
+//        float a = s.nextFloat();
+//        float b = s.nextFloat();
+//        System.out.println(a+b);
+
+//        int a = s.nextInt();
+//        int b = s.nextInt();
+//        int c = 0;
+//        System.out.println("Before swapping a = " + a + " b = "+ b +".");
+//        c = a;
+//        a = b;
+//        b = c;
+//        System.out.println("After swapping a = " + a + " b = "+ b +".");
+
+
+    }
+}
