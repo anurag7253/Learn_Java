854add8dbce61b76d4bba46087ff84e5523c00f7
Anurag Sharma <anuragsaitiya45@gmail.com>
2024-08-12 11:39:08 +0530

    update

---
+++ src/Diamond_Shape.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Diamond_Shape {
+    public static void main(String[] args){
+        System.out.println("Enter the number of Lines you want to Print :");
+//        Scanner s = new Scanner(System.in);
+          int num = 6;
+
+          for(int i=0; i<=num; i++){
+              System.out.println("*");
+          }
+
+    }
+}
---
+++ src/Pyramid_Number_Pattern.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pyramid_Number_Pattern {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num = 5;
+
+        for(int i=1 ; i <=num; i++){
+            System.out.print(i);
+        }
+
+
+    }
+
+}
