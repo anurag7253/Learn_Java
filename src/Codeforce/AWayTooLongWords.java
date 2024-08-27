6c9367f8dc5c00d489af109f583430fb7a918c28
Anurag Sharma <anuragsaitiya45@gmail.com>
2024-08-27 14:38:50 +0530

    27-08-2024

---
+++ src/Codeforce/AWayTooLongWords.java
@@ -0,0 +1,8 @@
+package Codeforce;
+import java.util.Scanner;
+public class AWayTooLongWords {
+    public static void main(String[] args){
+        String name = "Anurag Sharma";
+        System.out.println(name);
+    }
+}
