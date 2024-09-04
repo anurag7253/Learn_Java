d97148e3d8198af24e758ddb108fff48ae7851c6
Anurag Sharma <anuragsaitiya45@gmail.com>
2024-09-04 18:01:31 +0530

    reqi

---
+++ src/aRRay/array_1.java
@@ -0,0 +1,18 @@
+package aRRay;
+
+import java.sql.SQLOutput;
+
+public class array_1 {
+    public static void main(String[] args){
+
+        String name = "Anurag Sharma";
+
+        int[] roll = new int[10];
+        String[] names = {"anurag","sharma","bittu","sharma","rahul","kushawah"};
+        int len = names.length;
+        for(int i=0;i<len;i++){
+         System.out.println(names[i]);
+        }
+
+    }
+}
